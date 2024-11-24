ab4fd1fb14821f1d6bf29cffa3440861b47c66be
andrecosta14 <ac.14@live.com.pt>
2024-11-24 23:54:17 +0000

    v1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const AuthController = require('../controllers/authController');
+const authController = require('../controllers/authController');
 const { verifyToken, getUserInfo } = require('../utils/authUtils');
 
 const router = express.Router();
 
-router.post('/login', AuthController.login);
+router.post('/login', authController.login);
 router.get('/user', verifyToken, getUserInfo);
 
 module.exports = router;
