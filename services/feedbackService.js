098962ab08571d786a8f9ccca671ba3093511eef
andrecosta14 <ac.14@live.com.pt>
2025-01-02 12:59:32 +0000

    feedbackService added

---
+++ services/feedbackService.js
@@ -0,0 +1,45 @@
+const feedbackRepository = require('../data/feedbackRepository');
+
+class FeedbackService {
+    async createFeedback(data) {
+        return await feedbackRepository.create(data);
+    }
+
+    async getAllFeedbacks() {
+        return await feedbackRepository.findAll();
+    }
+
+    async getFeedbackById(id) {
+        const feedback = await feedbackRepository.findById(id);
+        if (!feedback) {
+            throw new Error('Feedback não encontrado.');
+        }
+        return feedback;
+    }
+
+    async findFeedbackByAvaliacaoId(avaliacaoId) {
+        const feedback = await feedbackRepository.findFeedbackByAvaliacaoId(avaliacaoId);
+        if (!feedback) {
+            throw new Error('Feedback não encontrado.');
+        }
+        return feedback;
+    }
+
+    async updateFeedback(id, data) {
+        const feedback = await feedbackRepository.updateById(id, data);
+        if (!feedback) {
+            throw new Error('Feedback não encontrado.');
+        }
+        return feedback;
+    }
+
+    async deleteAvaliacao(id) {
+        const avaliacao = await feedbackRepository.deleteById(id);
+        if (!avaliacao) {
+            throw new Error('Feedback não encontrado.');
+        }
+        return avaliacao;
+    }
+}
+
+module.exports = new FeedbackService();
